d23699628d84a123f5fdd4ab7dca066f09a6b4fb
Adrian <mendoza1975adrian@gmail.com>
2025-09-05 09:55:33 -0500

    subiendo ejercicio

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre = input("Ingrese su nomnbre: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, biemvenido al curso FA de {carrera}")
+
+ejer1()
---
+++ semana3C#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana3C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey(); //realizando detenimiento de consola
+            
+            static void ejer1();
+            {
+                
+                    string nombre, carrera;
+
+                    Console.Write("Ingrse su nombre");
+                    nombre = 
+        
+            
+            }
+    }
+}

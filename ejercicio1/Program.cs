0c1ca54bafbb3828627e00f6582591edcf830998
Adrian <mendoza1975adrian@gmail.com>
2025-09-26 10:08:02 -0500

    xdddnadj

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese número entero: ");
+            int num = int.Parse(Console.ReadLine());
+
+            while (num <= 0) 
+            {
+                Console.Write("Error. Ingrese numero entero: ");
+                num = int.Parse(Console.ReadLine());
+            }
+
+            int i = 1;
+            Console.WriteLine();
+            while (i <= 12)
+            {
+                Console.WriteLine($"{num} % {i} = {num*i}");
+                i++;
+            }
+        }
+    }
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int si = 0, sp = 0;
+            while (true)
+            {
+                Console.Write("Ingrese un número positivo: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == 0) break;
+                if (num < 0)
+                {
+                    Console.WriteLine("Error. Ingrese solo positivos");
+                    Console.WriteLine();
+                    continue;
+                }
+
+                if (num % 2 == 0) sp += num;
+                else si += num;
+            } 
+            Console.WriteLine("\nSuma de pares: "+sp);
+            Console.WriteLine("Suma de impares "+si);
+            Console.ReadKey();
+        }
+    }
+}
